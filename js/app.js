a25e789a9cd10c97f389892f7e761d1b27638227
马盼 <pan.ma@qingclass.com>
2017-05-07 09:03:29 +0800

    add router

--- js/app.js
+++ js/app.js
@@ -1,16 +1,36 @@
 (function(){
 	'use strict';
 
-	angular.module('dianshu',['ngMaterial'])
+	angular.module('dianshu',['ngMaterial','ui.router'])
 		.config(themeConfigure)
+		.config(routeConfigure)
 
 
 	function themeConfigure($mdThemingProvider){
 
 	  	$mdThemingProvider.theme('default')
-	    	.primaryPalette('red')
+	    	.primaryPalette('cyan')
 	    	.accentPalette('orange');
 
+	}
+
+
+	function routeConfigure($stateProvider, $urlRouterProvider){
+		
+		$stateProvider
+			.state('main',{
+				url: '/',
+				templateUrl: 'views/main.html',
+				abstract: true,
+				controller: angular.noop() 
+			})
+			.state('main.home',{
+				url: 'home',
+				templateUrl: 'views/home.html',
+				controller: angular.noop()
+			})
+
+		$urlRouterProvider.otherwise('/home');
 	}
 
 })()
---
+++ js/header.js
@@ -0,0 +1,9 @@
+(function(){
+	'use strict';
+
+	angular.module('dianshu')
+		.component('dhHeader',{
+			templateUrl: 'views/header.html'
+		})
+
+})()
